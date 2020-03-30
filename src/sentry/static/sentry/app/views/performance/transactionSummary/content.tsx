3aa7e5fc0936802f7ba0cf6381373f0b7aa6650d
Alberto Leal <mail4alberto@gmail.com>
2020-03-30 16:51:43 -0400

    move KeyTransactionButton into its own file

--- src/sentry/static/sentry/app/views/performance/transactionSummary/content.tsx
+++ src/sentry/static/sentry/app/views/performance/transactionSummary/content.tsx
@@ -2,24 +2,17 @@ import React from 'react';
 import {Location} from 'history';
 import styled from '@emotion/styled';
 
-import withApi from 'app/utils/withApi';
-import {Client} from 'app/api';
 import {Organization} from 'app/types';
-import {t} from 'app/locale';
-import space from 'app/styles/space';
-import theme from 'app/utils/theme';
 import overflowEllipsis from 'app/styles/overflowEllipsis';
 import EventView from 'app/utils/discover/eventView';
 import {ContentBox, HeaderBox} from 'app/views/eventsV2/styles';
 import Tags from 'app/views/eventsV2/tags';
 import EventsV2 from 'app/utils/discover/eventsv2';
-import Button from 'app/components/button';
-import {IconStar} from 'app/icons';
-import {saveKeyTransaction, deleteKeyTransaction} from 'app/actionCreators/performance';
 
 import SummaryContentTable from './table';
 import Breadcrumb from './breadcrumb';
 import UserStats from './userStats';
+import KeyTransactionButton from './keyTransactionButton';
 
 const TOP_SLOWEST_TRANSACTIONS = 5;
 
@@ -102,137 +95,6 @@ class SummaryContent extends React.Component<Props> {
   }
 }
 
-type KeyTransactionButtonProps = {
-  api: Client;
-  eventView: EventView;
-  organization: Organization;
-  transactionName: string;
-};
-
-type KeyTransactionButtonState = {
-  isLoading: boolean;
-  keyFetchID: symbol | undefined;
-  error: null | string;
-
-  isKeyTransaction: boolean;
-};
-
-const KeyTransactionButton = withApi(
-  class KeyTransactionButtonInner extends React.Component<
-    KeyTransactionButtonProps,
-    KeyTransactionButtonState
-  > {
-    state: KeyTransactionButtonState = {
-      isLoading: true,
-      keyFetchID: undefined,
-      error: null,
-
-      isKeyTransaction: false,
-    };
-
-    componentDidMount() {
-      this.fetchData();
-    }
-
-    componentDidUpdate(prevProps: KeyTransactionButtonProps) {
-      const orgSlugChanged = prevProps.organization.slug !== this.props.organization.slug;
-      const projectsChanged =
-        prevProps.eventView.project.length === 1 &&
-        this.props.eventView.project.length === 1 &&
-        prevProps.eventView.project[0] !== this.props.eventView.project[0];
-
-      if (orgSlugChanged || projectsChanged) {
-        this.fetchData();
-      }
-    }
-
-    fetchData = () => {
-      const {organization, eventView, transactionName} = this.props;
-
-      const projects = eventView.project as number[];
-
-      if (projects.length !== 1) {
-        return;
-      }
-
-      const url = `/organizations/${organization.slug}/is-key-transactions/`;
-      const keyFetchID = Symbol('keyFetchID');
-
-      this.setState({isLoading: true, keyFetchID});
-
-      this.props.api
-        .requestPromise(url, {
-          method: 'GET',
-          includeAllArgs: true,
-          query: {
-            project: projects.map(id => String(id)),
-            transaction: transactionName,
-          },
-        })
-        .then(([data, _, _jqXHR]) => {
-          if (this.state.keyFetchID !== keyFetchID) {
-            // invariant: a different request was initiated after this request
-            return;
-          }
-
-          this.setState({
-            isLoading: false,
-            keyFetchID: undefined,
-            error: null,
-            isKeyTransaction: !!data?.isKey,
-          });
-        })
-        .catch(err => {
-          this.setState({
-            isLoading: false,
-            keyFetchID: undefined,
-            error: err.responseJSON.detail,
-            isKeyTransaction: false,
-          });
-        });
-    };
-
-    toggleKeyTransaction = () => {
-      this.setState((prevState: KeyTransactionButtonState) => {
-        const nextIsKeyTransaction = !prevState.isKeyTransaction;
-
-        const {eventView, api, organization, transactionName} = this.props;
-
-        const projects = eventView.project as number[];
-
-        if (nextIsKeyTransaction) {
-          saveKeyTransaction(api, organization.slug, projects, transactionName);
-        } else {
-          deleteKeyTransaction(api, organization.slug, projects, transactionName);
-        }
-
-        return {
-          isKeyTransaction: !prevState.isKeyTransaction,
-        };
-      });
-    };
-
-    render() {
-      const {isKeyTransaction, isLoading} = this.state;
-
-      if (this.props.eventView.project.length !== 1 || isLoading) {
-        return null;
-      }
-
-      return (
-        <Button onClick={this.toggleKeyTransaction}>
-          <StyledIconStar
-            size="xs"
-            color={isKeyTransaction ? theme.yellow : undefined}
-            solid={!!isKeyTransaction}
-          />
-          {t('Key Transaction')}
-        </Button>
-      );
-    }
-  }
-);
-
 const StyledTitleHeader = styled('span')`
   font-size: ${p => p.theme.headerFontSize};
   color: ${p => p.theme.gray4};
@@ -249,10 +111,6 @@ const Side = styled('div')`
 const KeyTransactionContainer = styled('div')`
   display: flex;
   justify-content: flex-end;
-`;
-
-const StyledIconStar = styled(IconStar)`
-  margin-right: ${space(1)};
 `;
 
 export default SummaryContent;
