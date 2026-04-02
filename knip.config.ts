4b2f7a1769a24c0bc23f7c12f0713dc2fb08d04e
Scott Cooper <scttcper@gmail.com>
2026-04-02 13:58:31 -0700

    deps(ui): Upgrade to typescript 6, bump tsgo (#110151)
    removing the configs it told me to remove and a few extras we don't use
    like downlevelIteration
    
    https://devblogs.microsoft.com/typescript/announcing-typescript-6-0/

--- knip.config.ts
+++ knip.config.ts
@@ -70,13 +70,12 @@ const config: KnipConfig = {
   ignoreExportsUsedInFile: isProductionMode,
   ignoreDependencies: [
     'core-js',
+    'tslib', // subdependency of many packages, declare the latest version
     'jest-environment-jsdom', // used as testEnvironment in jest config
     'swc-plugin-component-annotate', // used in rspack config, needs better knip plugin
     '@swc/plugin-emotion', // used in rspack config, needs better knip plugin
     'buffer', // rspack.ProvidePlugin, needs better knip plugin
     'process', // rspack.ProvidePlugin, needs better knip plugin
-    '@types/webpack-env', // needed to make require.context work
-    '@types/gtag.js', // needed for global `gtag` namespace typings
     '@babel/preset-env', // Still used in jest
     '@babel/preset-react', // Still used in jest
     '@babel/preset-typescript', // Still used in jest
